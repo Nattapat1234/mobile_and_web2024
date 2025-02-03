89465429f1b0b0d4fa1205e6a1f373dc4e23271a
Nattapat1234 <Nattapat.ph@kkumail.com>
2025-02-03 20:29:44 +0700

    edit deploy

--- work6/src/index.js
+++ work6/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './work6';
-import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/mobile_and_web2024/work6">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
